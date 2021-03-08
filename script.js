2e7e3f6fc81bba7b4f43af0e08195d62999437d9
Lucas Barros <lucasbarrosez@gmail.com>
2021-03-08 10:59:03 -0300

    validacao personalizada

--- script.js
+++ script.js
@@ -1,43 +1,77 @@
 const fields = document.querySelectorAll("[required]")
 
-console.log(fields)
-
-function customValidation(event) {
-    const field = event.target;
+function customValidation(field) {
 
     function verifyErrors() {
         let foundError = false;
 
-        for (const error in field.validity) {
-            if (error != "customError" && field.validity[error]) {
+        for (let error in field.validity) {
+            if (field.validity[error] && !field.validity.valid) {
                 foundError = true;
             }
         }
         return foundError;
     }
-    const error = verifyErrors();
-    console.log("Error exists: ", error);
 
-    if (error) {
-        field.setCustomValidity("Esse campo é obrigatório");
-    }
-    else {
-        field.setCustomValidity("");
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo"
+            },
+            email: {
+                valueMissing: "Email é obrigatório",
+                typeMisMatch: "Por favor, preencha um email válido"
+            }
+        }
+        return messages[field.type][typeError]
     }
 
-    field.setCustomValidity("Esse campo é obrigatório")
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelectorAll("span.error");
+
+        if (message) {
+            spanError.classList.add("active");
+            spanError.innerHTML = message
+        }
+        else {
+            spanError.classList.remove("active");
+            spanError.innerHTML = "";
+        }
+    }
+
+    return function () {
+        const eror = verifyErrors();
+
+        if (error) {
+            const message = customMessage(error);
+
+            field.style.borderColor = "red";
+            setCustomMessage(message);
+        }
+        else {
+            field.style.borderColor = "green";
+            setCustomMessage();
+        }
+    }
+}
+
+function customValidation(event) {
+    const field = evend.target;
+    const validation = validateField(field);
+
+    validation();
 }
 
 for (field of fields) {
-    field.addEventListener("invalid", customValidation)
+    field.addEventListener("invalid", event => {
+        event.preventDefault();
+        customValidation(event);
+    });
+    field.addEventListener("blur", customValidation);
 }
 
+document, querySelector("form").addEventListener("submit", event => {
+    console.log("enviar formulário");
 
-
-
-document.querySelector("form")
-    .addEventListener("submit", event => {
-        console.log("enviar o formulario")
-
-        event.preventDefault()
-    })
+    event.preventDefault();
+});
